c86c94cf672b3d1fb568ce2f39bb359d47d72dca
Sergiu Groza <serioja90@gmail.com>
2012-09-30 03:34:06 +0200

    Figures definitions
    - changed the POINT structure definition
    - added definition for the COLOR structure
    - added definition for the LINE structure

--- include/figures.h
+++ include/figures.h
@@ -3,13 +3,33 @@
  * that will be used as the basic tools for models designing.
  */
 
- typedef struct _2dpoint{
- 	float x;
- 	float y;
- }2DPOINT;
+/**
+ * The structure for containing color data.
+ * All the components values will be stored as float data, because
+ * by float we can have an better definition of the color.
+ */
+typedef struct{
+	float r; /* red component */
+	float g; /* green component */
+	float b; /* blue component */
+}COLOR;
 
- typedef struct _3dpoint{
- 	float x;
- 	float y;
- 	float z;
- }3DPOINT;
+/**
+ * The structure that allows to contain a point info.
+ */
+ typedef struct{
+ 	float x; /* x coordinate */
+ 	float y; /* y coordinate */
+ 	float z; /* z coordinate */
+ 	COLOR color; /* the point color */
+ }POINT;
+
+/**
+ * The structure for storing the line data.
+ * A line has a start point, an end point and a color.
+ */
+ typedef struct{
+ 	POINT a; /* start point */
+ 	POINT b; /* end point */
+ 	COLOR color; /* the line color */
+ }LINE;
