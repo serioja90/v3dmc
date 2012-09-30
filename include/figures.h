ae61364e7ec168e648aca89fd3c553ccd7989073
Sergiu Groza <serioja90@gmail.com>
2012-09-30 03:14:49 +0200

    * added the 'include' and 'lib' directories * added the 'figures.h' header file that should contain the structure definitions of the primitives * added the 'figures.c' file that should contain the definitions of the methods for creation and initialization of the primitives structures

---
+++ include/figures.h
@@ -0,0 +1,17 @@
+/**
+ * Copyright Groza Sergiu.
+ *
+ * This file contains the structure definitions of the primitives
+ * that will be used as the basic tools for models designing.
+ */
+
+ typedef struct _2dpoint{
+ 	float x;
+ 	float y;
+ }2DPOINT;
+
+ typedef struct _3dpoint{
+ 	float x;
+ 	float y;
+ 	float z;
+ }3DPOINT;
---
+++ lib/figures.c
@@ -0,0 +1,8 @@
+/**
+ * Copyright Groza Sergiu.
+ *
+ * This file is used for the 'libfigures.so' dynamic library creation
+ * and contains the definitions of the methods that allows to initialize
+ * and create new instances of the structures declared inside the 
+ * 'include/figures.h' header.
+ */
